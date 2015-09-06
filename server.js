2c68f0c94173d92e59cbd58a36d3f24ab80cfcd0
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2015-09-05 21:14:41 -0400

    Refactoring the project structure to enable understandability

--- server.js
+++ server.js
@@ -5,23 +5,21 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./controller/index');
+var users = require('./controller/users');
 
 var app = express();
 var port = 8000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.use('/', routes);
 app.use('/users', users);
@@ -58,5 +56,6 @@ app.use(function(err, req, res, next) {
 });
 
 app.listen(port);
+console.log("Listening at port " + port);
 
 module.exports = app;
