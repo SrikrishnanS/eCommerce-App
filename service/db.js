b389624f1a89f0750d909f9417d74b2a0c96958c
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2015-09-06 18:24:40 -0400

    Removing the connection open and end statements

--- service/db.js
+++ service/db.js
@@ -9,16 +9,16 @@ var connection = mysql.createConnection({
 
 module.exports = {
 	authenticate : function(username, password, res) {
-			connection.connect();
 			connection.query('SELECT U.FULL_NAME, U.USERNAME, U.PASSWORD, R.DESCRIPTION FROM COMM_USERS U, COMM_ROLES R, COMM_USER_ROLES UR WHERE U.ID = UR.USER_ID AND R.ID=UR.ROLE_ID  AND U.USERNAME = ? AND U.PASSWORD = ?',[username,password], function(err, rows, fields) {
 			
-			connection.end();
 			if (err) throw err;
 			console.log("length"+rows.length);
 			if(rows.length == 0)
-				res.redirect('login/failed');
-			/*else if (rows.length == 1)
-				res.render('home',rows[0]);*/
+				res.redirect('/login/failed');
+			else if (rows.length == 1) {
+				user = rows[0];
+				res.render('home',user);
+			}
 		});
 		
 	}
