c0727c12ffdcf0b25cb96d9e330252fd4dc1111d
Srikrishnan S <srikrishnans@sase.ssn.edu.in>
2015-09-06 19:13:36 -0400

    Adding the id of the user in the query

--- service/db.js
+++ service/db.js
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
 
 module.exports = {
 	authenticate : function(username, password, res) {
-			connection.query('SELECT U.FULL_NAME, U.USERNAME, U.PASSWORD, R.DESCRIPTION FROM COMM_USERS U, COMM_ROLES R, COMM_USER_ROLES UR WHERE U.ID = UR.USER_ID AND R.ID=UR.ROLE_ID  AND U.USERNAME = ? AND U.PASSWORD = ?',[username,password], function(err, rows, fields) {
+			connection.query('SELECT U.ID, U.FULL_NAME, U.USERNAME, U.PASSWORD, R.DESCRIPTION FROM COMM_USERS U, COMM_ROLES R, COMM_USER_ROLES UR WHERE U.ID = UR.USER_ID AND R.ID=UR.ROLE_ID  AND U.USERNAME = ? AND U.PASSWORD = ?',[username,password], function(err, rows, fields) {
 			
 			if (err) throw err;
 			console.log("length"+rows.length);
