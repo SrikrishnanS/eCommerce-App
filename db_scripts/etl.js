b02ec0e268bf399d03ee424551eefe2b5b7b4692
Srikrishnan <srikriss@andrew.cmu.edu>
2015-10-18 21:29:21 -0400

    Changing the parser to parse new elements in the new dump file

--- db_scripts/etl.js
+++ db_scripts/etl.js
@@ -4,7 +4,7 @@
 var file = require('fs');
 var lazy = require("lazy");
 
-var productSourceFile = "./src/Project2Data.txt";
+var productSourceFile = "./src/amazon-meta.txt";
 var productDumpFile = "./src/product-dump.csv";
 var categoryDumpFile = "./src/category-dump.csv";
 
@@ -13,46 +13,56 @@ var idExp = /Id:(\s*)(\d+)/
 var asinExp = /ASIN:(\s*)(\w+)/
 var titleExp = /(\s*)title:(\s*)(.+)/
 var groupExp = /(\s*)group:(\s*)(.+)/
+var salesrankExp = /(\s*)salesrank:(\s*)(.+)/
+var similarExp = /(\s*)similar:(\s*)(\d+)(\s*)(.+)/
 var categoryExp = /(\s*)\|(.+)/
-var product = [];
+var product = null;
 
 var itemNumber = -1;
+
+var productDumpStream = file.createWriteStream(productDumpFile);
+var categoryDumpStream = file.createWriteStream(categoryDumpFile);
 
 new lazy(file.createReadStream(productSourceFile)).lines.forEach(function(item){ //Extract and transform operation
     var line = item.toString();
     if(line.match(idExp)) { //For Ids
-    	itemNumber++;
-    	product.push(new Object());
-    	product[itemNumber].ID = parseInt(line.match(idExp)[2]);
+
+        if(product != null) {
+            productDumpStream.write(product.ID+",");
+            productDumpStream.write(product.asin+",");
+            productDumpStream.write(product.title+",");
+            productDumpStream.write(product.group+",");
+            productDumpStream.write(product.salesrank+",");
+            productDumpStream.write(product.similar+"\n");
+            var category = product.categories;
+            for(chain in category) {
+                categoryDumpStream.write(product.ID+",");
+                categoryDumpStream.write(category[chain]+"\n");
+            }
+        }
+        product = new Object();
+    	product.ID = parseInt(line.match(idExp)[2]);
     }
     else if(line.match(asinExp)) { //For ASINs
-    	product[itemNumber].asin = line.match(asinExp)[2];
+    	product.asin = line.match(asinExp)[2];
     }
     else if(line.match(titleExp)) { //For titles
-    	product[itemNumber].title = line.match(titleExp)[3];
+    	product.title = line.match(titleExp)[3];
     }
     else if(line.match(groupExp)) { //For groups
-    	product[itemNumber].group = line.match(groupExp)[3];
+    	product.group = line.match(groupExp)[3];
+    }
+    else if(line.match(salesrankExp)) { //For sales rank
+        product.salesrank = line.match(salesrankExp)[3];
+    }
+    else if(line.match(similarExp)) { //For similar
+        product.similar = line.match(similarExp)[5];
     }
     else if(line.match(categoryExp)) {//For categories
-    	product[itemNumber].categories = product[itemNumber].categories || []; //check if array already exists, or else create it
-    	product[itemNumber].categories.push(line.match(categoryExp)[2]); //push the new category for the particular product
+    	product.categories = product.categories || []; //check if array already exists, or else create it
+    	product.categories.push(line.match(categoryExp)[2]); //push the new category for the particular product
     }
-}).on('pipe', function() { //Load operation
-    var productDumpStream = file.createWriteStream(productDumpFile);
-    var categoryDumpStream = file.createWriteStream(categoryDumpFile);
-    for(item in product) {
-    	productDumpStream.write(product[item].ID+",");
-    	productDumpStream.write(product[item].asin+",");
-    	productDumpStream.write(product[item].title+",");
-    	productDumpStream.write(product[item].group+"\n");
-
-    	var category = product[item].categories;
-    	for(chain in category) {
-    		categoryDumpStream.write(product[item].ID+",");
-    		categoryDumpStream.write(category[chain]+"\n");
-    	}
-    }
+}).on('pipe', function() { //End the streams
     productDumpStream.end();
     categoryDumpStream.end();
 });
