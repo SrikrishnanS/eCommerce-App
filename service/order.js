ffacafb2fe2bebf9ebc59129c98a9e7e6a3bbaef
Srikrishnan <srikriss@andrew.cmu.edu>
2015-11-02 19:37:53 -0500

    Adding the rollback statements in the sql transaction statements in case of any error in one of the queries

--- service/order.js
+++ service/order.js
@@ -38,8 +38,16 @@ module.exports = {
 			statement = 'SELECT QUANTITY_REM FROM COMM_PRODUCT_WAREHOUSE WHERE ID = ?;';
 			connection.query(statement,[productId], function(err, rows, fields) {
   			
-  				if (err) 
-  					throw err;
+  				if (err) {
+  					return connection.rollback(function(){
+						throw err;
+						jsonResponse = {
+			  				"message" : "Could not complete the transaction"
+			  			};
+			  			callback(jsonResponse);
+			  			return;
+					});
+  				}
   				quantity = rows[0].QUANTITY_REM;
 
   				if(quantity == 0) {
@@ -53,20 +61,45 @@ module.exports = {
 				statement = 'update COMM_PRODUCT_WAREHOUSE SET QUANTITY_REM = QUANTITY_REM - 1 WHERE ID=?;';
 				connection.query(statement,[productId], function(err, rows, fields) {
 	  			
-	  				if (err) 
-	  					throw err;
+	  				if (err) {
+	  					return connection.rollback(function(){
+							throw err;
+							jsonResponse = {
+				  				"message" : "Could not complete the transaction"
+				  			};
+				  			callback(jsonResponse);
+				  			return;
+						});
+	  				}
 	  				console.log('Updated warehouse');
 
 	  				//query 3 - if product is available and quantity avaiable decremented, increment quantity sold
 					statement = 'update COMM_PRODUCT_ORDERS SET QUANTITY_SOLD = QUANTITY_SOLD + 1 WHERE ID=?;';
 					connection.query(statement,[productId], function(err, rows, fields) {
 
-		  				if (err) 
-		  					throw err;
+		  				if (err) {
+		  					return connection.rollback(function(){
+								throw err;
+								jsonResponse = {
+					  				"message" : "Could not complete the transaction"
+					  			};
+					  			callback(jsonResponse);
+					  			return;
+							});
+		  				}
+
 		  				console.log('Updated sold information');
 		  				connection.commit(function(err) {
-							if (err) 
-			  					throw err;
+							if (err) {
+								return connection.rollback(function(){
+									throw err;
+									jsonResponse = {
+						  				"message" : "Could not complete the transaction"
+						  			};
+						  			callback(jsonResponse);
+						  			return;
+								});
+							}
 							console.log('Transction complete');
 							jsonResponse = {
 			  					"message" : "The purchase has been made successfully"
